c26d1c7f892e387bda794a190a9732c19f2539af
John Tucker <john@larkintuckerllc.com>
2018-10-08 06:56:56 -0400

    Display version

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Constants } from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{Constants.manifest.version}</Text>
       </View>
     );
   }
