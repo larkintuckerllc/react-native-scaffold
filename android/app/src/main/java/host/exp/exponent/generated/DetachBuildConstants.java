dd2fad305b58b89998d4b3eb1666bf55d17292d4
John Tucker <john@larkintuckerllc.com>
2018-10-08 12:46:43 -0400

    Post build

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp4650f1b7d98e40cb8081103f4ea9728f://192.168.1.38:19000";
 
 }
