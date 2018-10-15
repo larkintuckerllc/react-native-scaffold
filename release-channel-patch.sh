6574953da99fec5affed45419169bb9db00188a7
John Tucker <john@larkintuckerllc.com>
2018-10-15 14:49:13 -0400

    Fix for PR

--- release-channel-patch.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s/react-native-scaffold/react-native-scaffold?release-channel=${TRAVIS_PULL_REQUEST}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
-sed -i "s/default/${TRAVIS_PULL_REQUEST}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
