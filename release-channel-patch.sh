3c15def3d5f3913678c4488514866b6e9f3ba9f1
John Tucker <john@larkintuckerllc.com>
2018-10-17 12:16:51 -0400

    Partial

--- release-channel-patch.sh
+++ release-channel-patch.sh
@@ -1,2 +1,5 @@
-sed -i "s/react-native-scaffold/react-native-scaffold?release-channel=${TRAVIS_PULL_REQUEST}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
-sed -i "s/default/${TRAVIS_PULL_REQUEST}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+set -e
+set -x
+
+sed -i "s/react-native-scaffold/react-native-scaffold?release-channel=${CHANNEL}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+sed -i "s/default/${CHANNEL}/" android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
