488012ff6ac18f8ee63b59f22331189b1f75e4a4
John Tucker <jtucker@appirio.com>
2018-10-19 13:40:43 -0400

    Fixed

--- android.sh
+++ android.sh
@@ -1,6 +1,19 @@
 set -e
 set -x
 
+if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]
+then
+  CHANNEL=$TRAVIS_PULL_REQUEST
+else
+  if [ "$TRAVIS_BRANCH" = "master" ]
+  then
+    CHANNEL=`cat package.json | ./node_modules/.bin/json version`
+  else
+    CHANNEL=$TRAVIS_BRANCH
+  fi
+fi
+export CHANNEL=$CHANNEL
+expo publish --release-channel $CHANNEL
 sh release-channel-patch.sh
 cd android && ./gradlew assembleRelease && cd ..
 if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]
