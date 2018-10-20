99a702946c5ae13a9e3939607112d1261c3572df
John Tucker <jtucker@appirio.com>
2018-10-20 16:29:12 -0400

    Fix version scripts

--- version-expo.sh
+++ version-expo.sh
@@ -2,6 +2,6 @@ set -e
 set -x
 
 FILE_NAME="app.json"
-PACKAGE_VERSION=`cat package.json | ./node_modules/.bin/json version`
-json -I -f $FILE_NAME -e "this.expo.version=\"${PACKAGE_VERSION}\""
+PACKAGE_VERSION=`./node_modules/.bin/json -f package.json version`
+./node_modules/.bin/json -I -f $FILE_NAME -e "this.expo.version=\"${PACKAGE_VERSION}\""
 git add $FILE_NAME
