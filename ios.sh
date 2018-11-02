6e426263f0d886f0ae0bebf0f03b3b7b08073445
John Tucker <jtucker@appirio.com>
2018-11-02 16:04:51 -0400

    Try again

--- ios.sh
+++ ios.sh
@@ -1,9 +1,6 @@
 set -e
 set -x
 
-openssl aes-256-cbc -K $encrypted_91d26112053f_key -iv $encrypted_91d26112053f_iv -in development-cert.cer.enc -out development-cert.cer -d
-openssl aes-256-cbc -K $encrypted_91d26112053f_key -iv $encrypted_91d26112053f_iv -in development-key.p12.enc -out development-key.p12 -d
-openssl aes-256-cbc -K $encrypted_91d26112053f_key -iv $encrypted_91d26112053f_iv -in reactnativescaffolddev.mobileprovision.enc -out reactnativescaffolddev.mobileprovision -d
 npm install -g expo-cli@2.2.0
 expo login -u $EXPO_USERNAME -p $EXPO_PASSWORD
 expo publish
