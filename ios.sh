f051a6b511c902b9ec75536496561b5144dae081
John Tucker <jtucker@appirio.com>
2018-11-02 16:21:04 -0400

    Please

--- ios.sh
+++ ios.sh
@@ -1,7 +1,6 @@
 set -e
 set -x
 
-npm install -g expo-cli@2.2.0
-expo login -u $EXPO_USERNAME -p $EXPO_PASSWORD
-expo publish
-
+# npm install -g expo-cli@2.2.0
+# expo login -u $EXPO_USERNAME -p $EXPO_PASSWORD
+# expo publish
