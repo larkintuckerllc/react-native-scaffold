aff912d00b8c3ec6333b4c6b9327c1ba50443d23
John Tucker <jtucker@appirio.com>
2018-11-02 17:16:27 -0400

    Test KeyChain

--- ios.sh
+++ ios.sh
@@ -4,3 +4,7 @@ set -x
 # npm install -g expo-cli@2.2.0
 # expo login -u $EXPO_USERNAME -p $EXPO_PASSWORD
 # expo publish
+security create-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
