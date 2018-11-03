16485d52bf1a19206f824a0acc227556112c644b
John Tucker <jtucker@appirio.com>
2018-11-03 06:13:57 -0400

    Another

--- ios.sh
+++ ios.sh
@@ -11,8 +11,7 @@ security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # IMPORT ASSETS INTO KEY CHAIN
 security import AppleWWDRCA.cer -k ios-build.keychain -A
-security import development-cert.cer -k ios-build.keychain -A
-security import development-key.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
+security import development-cert.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
 security set-key-partition-list -S apple-tool:,apple: -s -k $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain > /dev/null
 # PROVISIONING PROFILE
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
