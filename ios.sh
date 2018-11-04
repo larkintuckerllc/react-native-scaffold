b17a22ce4758b1ebcda6e64e9ecda619b2952671
John Tucker <jtucker@appirio.com>
2018-11-03 21:35:03 -0400

    Upload

--- ios.sh
+++ ios.sh
@@ -42,4 +42,4 @@ cd ios && xcodebuild \
   -archivePath $PWD/build/react-native-scaffold.xcarchive \
   -exportOptionsPlist $PWD/build/react-native-scaffold.xcarchive/Info.plist \
   -exportPath $PWD/build | xcpretty && cd .. && exit ${PIPESTATUS[0]}
-
+node ios-aws.js
